98a53bf1567c3797478185bb6aa632b834910c73
veeroteen <vitalibyrd@gmail.com>
2022-09-29 20:36:56 +0700

    3

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
-#include <iostream>
-#include <vector>
 #include "DataModels/Engine.h"
-#include "nlohmann/json.hpp"
-#include <fstream>
-#include <random>
-#include "DataModels/Models/WordIndex.h"
-#include <algorithm>
-#include <cmath>
-
 
 
 int main() {
     Engine engine("config.json", "request.json" , "answer.json" );
     engine.work();
 
-
-
-
-
     return 0;
-
-
 }
